53a32be0127fc811d007ca10223ef1066dfc992f
Feazan <feazany@gmail.com>
2019-04-01 02:58:47 -0400

    Creating POST route

--- app.js
+++ app.js
@@ -6,20 +6,41 @@ var cassandra = require('cassandra-driver');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-var client = new cassandra.Client({ contactPoints: [ '130.245.170.207' ] });
+var client = new cassandra.Client({ contactPoints: [ '130.245.170.207' ], keyspace: 'hw5' });
 client.connect(function(err, result) {
-	console.log('Cassandra connected');
+	if (err) {
+		console.log('Error Occurred: ', err);
+	} else {
+		console.log('Cassandra Connected!');
+	}
 });
 
 app.get('/', function(req, res) {
-	res.send('Welcome to the Home Page!');
+	res.render('index');
 });
 
+var upsertInfo = 'INSERT INTO hw5.imgs(filename, contents) VALUES(?,?)';
 app.post('/deposit', function(req, res) {
 	var filename = req.body.filename;
 	var contents = req.body.contents;
 
 	// Insert into Cassandra
+	console.log('Filename: ', filename, ' Contents: ', contents);
+	client.execute(upsertInfo, [ filename, contents ], function(err, result) {
+		if (err) {
+			res.status(404).send({ msg: err });
+		} else {
+			console.log('Image Added!');
+			res.send({ status: 'OK' });
+		}
+	});
+});
+
+app.get('/retrieve', function(req, res) {
+	console.log('Inside retrieve route');
+	var filename = req.body.filename;
+
+	// Find filename in Cassandra and return it
 });
 
 app.listen(3000, function() {
